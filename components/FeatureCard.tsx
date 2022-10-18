0e46d06627f6b36ccdea63e8acd1e341b68710d6
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-10-18 11:31:06 +0600

    create banner

---
+++ components/FeatureCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type Props = {};
+
+const FeatureCard = (props: Props) => {
+  return (
+    <div className="w-full p-6 bg-[#EEEFFB]  rounded-lg border border-gray-200 shadow-md flex gap-2 items-center flex-row-reverse justify-end">
+      <div>
+        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 ">
+          সকল পণ্যে ২৩% ডিসকাউন্ট
+        </h5>
+        <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+          Shop Now
+        </p>
+      </div>
+      <div className="flex">
+        <img
+          height={100}
+          width={100}
+          className="rounded"
+          src="https://static.toiimg.com/photo/msid-68976835/68976835.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FeatureCard;
